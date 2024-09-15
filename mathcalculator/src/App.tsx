82f7722e4fbbe89ab4e379beb3daf0a4222e0a72
SChakraborty04 <shaanch04@yahoo.com>
2024-09-16 03:44:33 +0530

    initial commit

---
+++ mathcalculator/src/App.tsx
@@ -0,0 +1,29 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+
+
+import './index.css';
+import Home from './home';
+
+const paths = [
+    {
+        path: '/',
+        element: (
+          <Home/>
+        ),
+    },
+];
+
+const BrowserRouter = createBrowserRouter(paths);
+
+const App = () => {
+    return (
+    <MantineProvider>
+      <RouterProvider router={BrowserRouter}/>
+    </MantineProvider>
+    )
+};
+
+export default App;
---
+++ mathcalculator/src/components/constants.ts
@@ -0,0 +1,15 @@
+const SWATCHES=[
+    "#FFFFFF",//white
+    "#FF0000",//red
+    "#00FF00",//green
+    "#0000FF",//blue
+    "#FFFF00",//yellow
+    "#00FFFF",//cyan
+    "#FFA500",//orange
+    "#800080",//purple
+    "#808080",//gray
+    "#A52A2A",//brown
+    "#FFC0CB",//pink
+]
+
+export {SWATCHES}
